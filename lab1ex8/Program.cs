93ec64e07b7eb8dedb6cfac1b925b046be38cce1
uimkatali <b8bf6b5@gmail.com>
2023-09-18 13:29:12 +0300

    Add project files.

---
+++ lab1ex8/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace lab1ex8
+{
+    class Program
+    {
+        /*
+         Scrieti un program care interschimba valoarea a doua variabile intregi
+         */
+        static void Main(string[] args)
+        {
+            int a = 5; 
+            int b = 99;
+            int temp;
+            Console.WriteLine("Valori initiale: ");
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine("Interschimbare: ");
+            temp = a;
+            a= b;
+            b= temp;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+
+        }
+    }
+}
